4ac1d984dc7a148e474d67ad292ff2e75bbb7623
Thyago Oliveira <thyagoferreira_@outlook.com>
2022-01-29 17:09:35 -0300

    commit2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React, { Component } from 'react';
 import "./assets/App.css";
+import Header from './componentes/header/header';
 
-function App() {
-  return (
 
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="conteudo">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
