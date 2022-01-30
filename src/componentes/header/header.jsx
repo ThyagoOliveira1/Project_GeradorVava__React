20223d264e9aba19d1051e402a0ff985451b6f4b
Thyago Oliveira <thyagoferreira_@outlook.com>
2022-01-30 19:06:43 -0300

    Commit3

--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -4,17 +4,27 @@ import "./estilo.css";
 class Header extends Component {
   render() {
     return (
-        <header className="cabecalho">
-            <div className="container">
-                <h1 className="cabecalho_titulo">Team Maker Application</h1>
-                <nav className="cabecalho_nav">
-                    <a className="cabecalho_nav_link" href="#">Sobre</a>
-                    <a className="cabecalho_nav_link" href="#">Team Maker</a>
-                    <a className="cabecalho_nav_link" href="#">Patch Notes</a>
-                    <a className="cabecalho_nav_link" href="#">Contato</a>
-                </nav>
-            </div>
-        </header>
+      <header className="cabecalho">
+        <div className="cabecalho_container container">
+          <h1 className="cabecalho_titulo">Team Maker Application</h1>
+          <nav className="cabecalho_nav">
+            <ul className="cabecalho_nav_list">
+              <li className="cabecalho_nav_item">
+                <a href="#sobre">Sobre</a>
+              </li>
+              <li className="cabecalho_nav_item">
+                <a href="#teamMaker">Montar Times</a>
+              </li>
+              <li className="cabecalho_nav_item">
+                <a href="#patch_Notes">Patch Notes</a>
+              </li>
+              <li className="cabecalho_nav_item">
+                <a href="#contato">Contato</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     );
   }
 }
